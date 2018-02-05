43148c3aecbee330aec9ffba3e068844af139326
Piyush <pysingh09@gmail.com>
2018-02-05 09:36:13 +0530

    feat: added tone analyser

--- project/ibm_watson/views.py
+++ project/ibm_watson/views.py
@@ -19,39 +19,86 @@ class WatsonDashboard(View):
         return render(request, 'ibm_watson/index.html')
 
     def post(self, request):
-    	type_ = request.POST['optionsRadios']
-    	text = request.POST['text']
+      type_ = request.POST['optionsRadios']
+      text = request.POST['text']
+      if type_ == 'tone':
+        headers = {'content-type': 'text/plain;charset=utf-8'}
+        payload = text
+        res = requests.post(url='https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21',
+            data= payload,
+            headers= headers,
+            auth = ('14bd2691-f7a4-434a-b56b-0916f676b7d1', 'KlF71h0J4EYI'))
+        # tone = tone_analyzer.tone(text)
+        tone = res.json()
+        print("tone", tone)
+        if not tone['document_tone']['tones']:
+          html_data = '''
+              <div class="row">
+                <div class="card">
+                  <div class="card-body">
+                              <h5 class="card-title alert alert-danger">Error</h5>
+                              <p class="card-text">{0}</p>
+                            </div>
+                </div>
+              </div>
+          '''.format('Please provide a valid Input!')
+          return HttpResponse(html_data)
 
-    	# curl -X POST --user "afac5070-e852-4695-ad00-a576bf09af5a":"mE7wE5vQPZJd" --header "Content-Type: text/plain;charset=utf-8" --data-binary "@/home/laptop30/Downloads/profile.json" "https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13"
-    	headers = {'content-type': 'text/plain;charset=utf-8'}
-    	user = {"afac5070-e852-4695-ad00-a576bf09af5a":"mE7wE5vQPZJd"}
-    	payload = text
-    	res = requests.post(url='https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13',
-    			data= payload,
-    			headers= headers,
-    			auth = ('afac5070-e852-4695-ad00-a576bf09af5a', 'mE7wE5vQPZJd'))
-    	datap = res.json()
-    	if 'code' in datap:
-    		html_data = '''
-    			<div class="row">
-    				<div class="card">
-    					<div class="card-body">
+        html_data = '''
+            <div class="row">
+                <div class="col-sm-12">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title alert alert-success">Tone Insights</h5>
+                      <p class="card-text"></p>
+        '''
+        for sentence in tone['sentences_tone']:
+          for sentence_tone in sentence['tones']:
+            html_data += '''
+              <h3>{2}</h3>
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: {0}% ;" aria-valuenow="{0}" aria-valuemin="0" aria-valuemax="100">{1} {0}%</div>
+              </div>
+
+          '''.format(int(sentence_tone['score'] * 100), sentence['text'], sentence_tone['tone_name'])
+        html_data += '''
+              </div>
+            </div>
+            </div>
+          </div>
+        '''
+        return HttpResponse(html_data)
+
+      # curl -X POST --user "afac5070-e852-4695-ad00-a576bf09af5a":"mE7wE5vQPZJd" --header "Content-Type: text/plain;charset=utf-8" --data-binary "@/home/laptop30/Downloads/profile.json" "https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13"
+      headers = {'content-type': 'text/plain;charset=utf-8'}
+      user = {"afac5070-e852-4695-ad00-a576bf09af5a":"mE7wE5vQPZJd"}
+      payload = text
+      res = requests.post(url='https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13',
+          data= payload,
+          headers= headers,
+          auth = ('afac5070-e852-4695-ad00-a576bf09af5a', 'mE7wE5vQPZJd'))
+      datap = res.json()
+      if 'code' in datap:
+        html_data = '''
+          <div class="row">
+            <div class="card">
+              <div class="card-body">
                           <h5 class="card-title alert alert-danger">Error</h5>
                           <p class="card-text">{0}</p>
                         </div>
-    				</div>
-    			</div>
-    		'''.format(datap['error'])
-    		return HttpResponse(html_data)
-    	a = int(datap['personality'][0]['percentile'] * 100)
-    	b = int(datap['personality'][1]['percentile'] * 100)
-    	c = int(datap['personality'][2]['percentile'] * 100)
-    	d = int(datap['personality'][3]['percentile'] * 100)
-    	e = int(datap['personality'][4]['percentile'] * 100)
-    	word_count = datap['word_count']
+            </div>
+          </div>
+        '''.format(datap['error'])
+        return HttpResponse(html_data)
+      a = int(datap['personality'][0]['percentile'] * 100)
+      b = int(datap['personality'][1]['percentile'] * 100)
+      c = int(datap['personality'][2]['percentile'] * 100)
+      d = int(datap['personality'][3]['percentile'] * 100)
+      e = int(datap['personality'][4]['percentile'] * 100)
+      word_count = datap['word_count']
 
-    	html_data = '''
-    		<div class="row">
+      html_data = '''
+        <div class="row">
                     <div class="col-sm-12">
                       <div class="card">
                         <div class="card-body">
@@ -86,7 +133,7 @@ class WatsonDashboard(View):
                     </div>
                   </div>
         '''.format(a,b,c,d,e,word_count)
-    	return HttpResponse(html_data)
+      return HttpResponse(html_data)
 
 class WatsonPersonality(View):
     def get(self, request):
